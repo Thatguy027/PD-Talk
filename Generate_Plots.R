c39c7b2456f4947951001d69c0c214d71afb63d4
Stefan Zdraljevic <stefan@vpn-165-124-163-45.vpn.northwestern.edu>
2019-03-12 23:19:59 -0500

    initialize plot scripts and presentation

--- Generate_Plots.R
+++ Generate_Plots.R
@@ -3,9 +3,27 @@ try(setwd(dirname(rstudioapi::getActiveDocumentContext()$path)))
 # data folders
 arsenic_data <- "Arsenic/Data/"
 etoposide_data <- "Etoposide/Data/"
-ben1_data <- "201809_rappaport/Data/"
+ben1_data <- "201809_rappaport/Data/ben1_data/Final_Tables/"
 
 # source base theme and colors for plots
 source("Figure_Themes.R")
 
+# source scripts 
+source()
+
+# ben1
+
+# # # LOAD AND PROCESS INDELS
+ben1_variants <- data.table::fread(glue::glue("{ben1_data}TS9_ben-1_variants.tsv"))%>%
+  na.omit()
+
+gwa_mappings <-  data.table::fread(file = glue::glue("{ben1_data}TS5_GWA_processed_marker_mapping.tsv"))%>%
+  na.omit()%>%
+  dplyr::mutate(snpGT = ifelse(allele==-1,"REF", "ALT"))%>%
+  dplyr::select(snpMarker = marker, strain, trait, value, snpGT)%>%
+  dplyr::filter(trait == "q90.tof")%>%
+  dplyr::left_join(.,ben1_variants, by = "strain")
+
+
+# Arsenic
 
